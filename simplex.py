3f6c5f17ed49beb5d0c5af10721d7b89de8f617a
Eduardo V. Argenton <eduardov.argenton@outlook.com>
2022-10-27 15:57:06 -0300

    Simplex min + max s/ restricao especial

--- simplex.py
+++ simplex.py
@@ -2,34 +2,44 @@ import numpy as np
 
 class Simplex:
 
-    def __init__(self, num_rest, num_var):
+    def __init__(self, num_rest, num_var, fo_min = False):
         self.num_rest = num_rest
         self.num_var = num_var
-        self.num_lines = self.num_rest + 1
-        self.num_coloumns = self.num_var + self.num_rest + 2
+        self.num_lines = 0
+        self.num_coloumns = 0
+        self.fo_min = fo_min
         self.algorithms = []
-        self.generate_matrix()
-        matrix = self.get_algorithm()
-        matrix[0,0] = 1
-        for num_line, line in enumerate(matrix):
-            if(num_line == 0):
-                continue
-            line[num_var + num_line] = 1
-        
+    
+    
+    def insert_num_coloumns(self):
+        self.num_coloumns = self.num_var + self.num_rest + 2
     
 
-    def generate_matrix(self):
-        self.algorithms.append(np.zeros([self.num_lines, self.num_coloumns], dtype=float))
+    def insert_num_lines(self):
+        self.num_lines = self.num_rest + 1
 
 
     def insert(self,line, values):
         matrix = self.get_algorithm()
-        if(line == 0):
-            values = np.negative(values)
-            values[-1] = - values[-1]
         matrix[line][1:self.num_var + 1] = values[:-1]
         matrix[line][-1] = values[-1]
-    
+
+
+    def auto_insert_extras(self):
+        matrix = self.get_algorithm()
+        for i in range(1, self.num_lines):
+            matrix[i][self.num_var + i] = 1
+        if(self.fo_min):
+            matrix[0][0] = -1
+        
+        else:
+            matrix[0] = np.negative(matrix[0])
+            matrix[0][0] = 1
+
+
+    def generate_matrix(self):
+        self.algorithms.append(np.zeros([self.num_lines, self.num_coloumns], dtype=float))
+
 
     def insert_line(self, line, new_line):
         matrix = self.get_algorithm()
@@ -38,4 +48,82 @@ class Simplex:
 
     def get_algorithm(self, index = -1):
         return self.algorithms[index]
-        
+
+    
+    def select_pivot(self):
+        matrix = self.get_algorithm()
+        for elements in matrix[0][1:self.num_var + 1]:
+            if(elements < 0):
+                break
+        else:
+            return 0
+
+        pivot_candidates = {}
+        for i, element in enumerate(matrix[0]):
+            if(element < 0):
+                pivot_candidates[i] = element
+        if(self.fo_min):
+            pivot_coloumn = max(pivot_candidates)
+        else:
+            pivot_coloumn = min(pivot_candidates)
+        pivot_coloumn_values = []
+        for i,elements in enumerate(matrix):
+            if(i == 0):
+                continue
+            if(elements[pivot_coloumn] > 0):
+                pivot_coloumn_values.append(elements[self.num_coloumns - 1] / elements[pivot_coloumn])
+        pivot_line = np.nanargmin(pivot_coloumn_values) + 1
+        return {'line': pivot_line, 'coloumn': pivot_coloumn, 'element': matrix[pivot_line][pivot_coloumn]}
+    
+    
+    def execute(self):
+        self.auto_insert_extras()
+        while True:
+            matrix = self.get_algorithm()
+            pivot = self.select_pivot()
+            print(pivot)
+            if(pivot == 0):
+                break
+            self.generate_matrix()
+            new_pivot_line = matrix[pivot['line']] / pivot['element']
+            self.insert_line(pivot['line'], new_pivot_line)
+            for i in range(0, self.num_lines):
+                if(i == pivot['line']):
+                    continue
+                new_line = ((- matrix[i][pivot['coloumn']]) * new_pivot_line) + matrix[i]
+                self.insert_line(i, new_line)
+
+
+class SpecialSimplex(Simplex):
+
+    def __init__(self, num_rest, num_var, num_xf, num_a, fo_min = False):
+        super.__init__(num_rest, num_var, fo_min)
+        self.num_xf = num_xf
+        self.num_a = num_a
+
+
+    def insert_num_coloumns(self):
+        self.num_coloumns = self.num_var + self.num_xf + self.num_a + 2
+    
+
+    def insert_num_lines(self):
+        self.num_lines = self.num_rest + 2
+    
+
+class Factory:
+    
+    def create_simplex(self, num_rest, num_var, fo_min = False, num_xf = 0, num_a = 0):
+        if(num_a == 0):
+            simplex = Simplex(num_rest, num_var, fo_min)
+        else:
+            simplex = SpecialSimplex(num_rest, num_var, num_xf, num_a, fo_min)
+        simplex.insert_num_coloumns()
+        simplex.insert_num_lines()
+        simplex.generate_matrix()
+        return simplex
+
+    
+    
+    
+
+
--- teste.py
+++ teste.py
@@ -1,16 +1,22 @@
-from simple import *
-from simplex import Simplex
+from simplex import *
 
-num_var = 2
-num_rest = 2
-x = Simplex(num_rest, num_var)
-x.insert(0, [5,2,5])
-x.insert(1, [2,1,6])
-x.insert(2, [10,12,60])
-execute(x)
-print(x.get_algorithm())
+fact = Factory()
+simplex = fact.create_simplex(3,3, True)
+simplex.insert(0, [1,-2,-3,0])
+simplex.insert(1, [3,4,9,81])
+simplex.insert(2, [5,6,7,27])
+simplex.insert(3, [1,2,3,47])
+simplex.execute()
+
+for i in simplex.algorithms:
+    print(i)
+    print('=====================')
+
+# execute(x)
+#print(x.get_algorithm())
 # x.alter_line(0, np.subtract(x.matrix[0], x.matrix[1]))
 #x.alter_line(1, x.matrix[1] / 2)
 #print()
 #y = select_pivot(x)
 #print(y)
+
